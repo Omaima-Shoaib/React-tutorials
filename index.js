7a458239cbeda275867de98ad9035bd31b3bd918
Omaima-Shoaib <sim.omaimashoaib3023@alexu.edu.eg>
2023-11-16 08:35:41 +0200

    use redux-logger as middleware

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const redux = require("redux");
+const reduxLogger = require("redux-logger");
 const createStore = redux.createStore;
+//this is our middleware
+const logger = reduxLogger.createLogger();
 const BUY_CAKE = "BUY_CAKE";
 const BUY_ICECREAM = "BUY_ICECREAM";
 const combineReducers = redux.combineReducers;
+//used in order to pass the middleware as a parameter.
+const applyMiddleware = redux.applyMiddleware;
 //create Actions
 function buyCake() {
   return {
@@ -49,15 +54,14 @@ const iceCreamReducer = (state = initailIceCreameState, action) => {
   }
 };
 
-const rootReducer = combineReducers({ cake: cakeReducer,
-iceCream:iceCreamReducer
+const rootReducer = combineReducers({
+  cake: cakeReducer,
+  iceCream: iceCreamReducer,
 });
 //create store, store function accepts reducer as a parameter
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(logger));
 console.log("Initial state ", store.getState());
-const unsubscribe = store.subscribe(() =>
-  console.log("Updated state", store.getState())
-);
+const unsubscribe = store.subscribe(() => {});
 store.dispatch(buyCake());
 store.dispatch(buyIceCream());
 unsubscribe();
