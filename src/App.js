185cc56782fd27e2c820107760f07229a6161893
Omaima-Shoaib <sim.omaimashoaib3023@alexu.edu.eg>
2023-11-21 13:52:11 +0200

    useSelector and useDispatch hooks

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import CakeContainer from "./components/CakeContainer";
 import { Provider } from "react-redux";
 import store from "./components/redux/store";
+import HooksCakeContainer from "./components/HooksCakeContainer";
 function App() {
   return (
     <Provider store={store}>
     <div className="flex justify-center bg-slate-50 min-h-screen py-5">
       <div className="App flex flex-col  bg-gradient-to-b from-slate-200 to-slate-400 w-1/2 justify-center items-center rounded-lg shadow-lg border border-slate-300">
-        <CakeContainer></CakeContainer>
+        {/* <CakeContainer></CakeContainer> */}
+        <HooksCakeContainer></HooksCakeContainer>
 
       </div>
     </div>
